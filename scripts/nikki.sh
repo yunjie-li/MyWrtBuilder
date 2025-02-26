20d67e0696869fd012d48d987555e20532ab7c25
yunjie <yunjie-li@users.noreply.github.com>
2025-02-26 16:09:14 +0800

    Update nikki.sh

--- scripts/nikki.sh
+++ scripts/nikki.sh
@@ -1,3 +1,1 @@
-# tar -xvf packages/nikki_x86_64-openwrt-24.10.tar.gz
-
-curl -s -L https://github.com/nikkinikki-org/OpenWrt-nikki/raw/refs/heads/main/feed.sh | bash
+tar -xvf packages/nikki_x86_64-openwrt-24.10.tar.gz
