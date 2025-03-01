5f91e5c3b6822d0381d2a18678c292af5ef96331
yunjie <yunjie-li@users.noreply.github.com>
2025-03-02 02:07:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,90 +15,39 @@ PACKAGES="$PACKAGES luci-theme-argon luci-i18n-argon-config-zh-cn"
 
 # Diskman 磁盘管理
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-# CPU 占用限制
-# PACKAGES="$PACKAGES luci-i18n-cpulimit-zh-cn"
-# collectd 统计
-# PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 
 # 常用软件服务
 
 # OpenClash 代理
 PACKAGES="$PACKAGES luci-app-openclash"
-# Passwall 代理
-# PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-# frp 和 nps 内网穿透
-# PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
 
 # 常用的网络存储组件
 
 # 文件助手
-# PACKAGES="$PACKAGES luci-app-fileassistant"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # TTYD
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # UPNP
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
-# 硬盘休眠
-# PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
-# p910nd 打印服务器
-# PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
-# GoWebDav
-# PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
-# Samba 网络共享
-# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# qBittorrent 下载器
-# PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
-# Aria2 下载器
-# PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
-# FTP 服务器
-# PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 
-# Docker 组件
-# PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn"
-
-# VPN 类
-
-# IPSec VPN
-# PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
-# Zerotier
-# PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
-
-# 网络组件
-
-# EQoS 网速控制
-# PACKAGES="$PACKAGES luci-i18n-eqos-zh-cn"
-# 多线多拨与负载均衡
-# PACKAGES="$PACKAGES luci-app-syncdial luci-i18n-mwan3-zh-cn"
-# 宽带监控 Nlbwmon
-# PACKAGES="$PACKAGES luci-i18n-nlbwmon-zh-cn"
 
 # 界面翻译补全
-# PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
 
 # Packages 文件夹下的 ipk 包
-# PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
-PACKAGES="$PACKAGES adguardhome"
-PACKAGES="$PACKAGES luci-app-adguardhome"
 PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-lucky-zh-cn"
-
-
-# 一些自己需要的内核组件
-# PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
 
 # 一些其他可能有用的包
 
 # zsh 终端
 PACKAGES="$PACKAGES zsh"
 PACKAGES="$PACKAGES git"
+PACKAGES="$PACKAGES wget"
+PACKAGES="$PACKAGES wget-ssl"
 # Vim 完整版，带语法高亮
 PACKAGES="$PACKAGES vim-fuller"
-# X/Y/ZMODEM 文件传输
-# PACKAGES="$PACKAGES lrzsz"
-# OpenSSH
-# PACKAGES="$PACKAGES openssh-server openssh-client"
 # Netdata 系统监控界面
 PACKAGES="$PACKAGES netdata"
 
