4057f29a610ad83e9034b9c762ef0f2e146f1fd4
yunjie <yunjie-li@users.noreply.github.com>
2025-03-04 14:38:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 
 
 # 界面翻译补全
-PACKAGES="$PACKAGES luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-opkg-zh-cn"
 
 # Packages 文件夹下的 ipk 包
 PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
