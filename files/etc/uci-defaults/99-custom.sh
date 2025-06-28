dd37e1a20438ac3eb5b0c26f945bc08b1450a382
yunjie <yunjie-li@users.noreply.github.com>
2025-06-28 22:30:25 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -4,10 +4,24 @@
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 
+# LAN 网络设置
+uci set network.lan.proto='static'
 uci set network.lan.ipaddr='192.168.2.253'
+uci set network.lan.netmask='255.255.255.0'
+
+# WAN 网络设置
+uci set network.wan.proto='dhcp'
+uci set network.wan=interface
+uci set network.wan.device='eth3'
+
+uci set network.wan6=interface
+uci set network.wan6.device='eth3'
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
+
+# 设置 SSH
+uci set dropbear.@dropbear[0].Interface='lan'
 
 uci commit
 
