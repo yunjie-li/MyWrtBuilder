e647ed21e2065538e2ee40f08c5b0b408b8a1f45
yunjie <yunjie-li@users.noreply.github.com>
2025-07-13 11:29:53 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -6,19 +6,6 @@ echo "Current Path: $PWD"
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 
-# Clash Dev
-# wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
-# tar -zxvf clash-linux-amd64.tar.gz
-# rm -rf clash-linux-amd64.tar.gz
-# mv clash clash_dev
-
-# Clash TUN
-# VERSION=$(curl -sS https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version | awk 'NR==2')
-# wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-$VERSION.gz
-# gzip -d clash-linux-amd64-$VERSION.gz
-# rm -rf clash-linux-amd64-$VERSION.gz
-# mv clash-linux-amd64-$VERSION clash_tun
-
 # Clash Meta
 wget https://github.com/vernesong/OpenClash/raw/refs/heads/core/dev/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
@@ -32,3 +19,6 @@ mv clash clash_smart
 
 # Use clash_dev as default core
 mv clash_smart clash
+
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > files/etc/openclash/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > files/etc/openclash/GeoSite.dat
